a732f84b6fb3b13c977c0192227aad326efb3c43
Hải Đăng <haidang27082005@gmail.com>
2025-08-12 15:11:35 +0700

    asdf

--- src/types/product.ts
+++
@@ -1,9 +0,0 @@
-export interface ProductInput {
-    id: string;
-    name: string;
-    price: number;
-    image: string;
-    category?: string;
-    description?: string;
-    // thêm các trường khác nếu cần
-}
