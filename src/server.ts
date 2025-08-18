1ca6340abb09d0305065bc8d7bb5299bb1708c8e
Hải Đăng <haidang27082005@gmail.com>
2025-08-18 11:58:29 +0700

    sdfs

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,19 @@ dotenv.config();
 
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet'; // Thêm dòng này để import helmet
 import connectDB from './config/db';
 import authRoutes from './routes/auth';
 
 connectDB();
 
 const app = express();
+
+// Sử dụng helmet trước các route của bạn để áp dụng các chính sách bảo mật
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
