e6e397da76b068d001d60c9959580f4db913d05b
Hải Đăng <haidang27082005@gmail.com>
2025-08-21 22:05:19 +0700

    wwwer

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,4 @@
 import mongoose, { Document, Schema } from "mongoose";
-import bcrypt from "bcryptjs";
 
 export interface IUser extends Document {
     name: string;
@@ -17,18 +16,20 @@ const UserSchema: Schema<IUser> = new Schema(
     { timestamps: true }
 );
 
-UserSchema.pre<IUser>("save", async function (next) {
-    if (this.isModified("password")) {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-    }
-    next();
-});
+// ❌ Bỏ middleware hash password
+// UserSchema.pre<IUser>("save", async function (next) {
+//     if (this.isModified("password")) {
+//         const salt = await bcrypt.genSalt(10);
+//         this.password = await bcrypt.hash(this.password, salt);
+//     }
+//     next();
+// });
 
+// matchPassword chỉ so sánh plain text
 UserSchema.methods.matchPassword = async function (
     enteredPassword: string
 ): Promise<boolean> {
-    return await bcrypt.compare(enteredPassword, this.password);
+    return enteredPassword === this.password;
 };
 
 export default mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
