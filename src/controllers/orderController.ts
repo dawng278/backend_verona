2c4902151fd105e3bdaaba096b107b75e38e6b6b
Hải Đăng <haidang27082005@gmail.com>
2025-08-24 17:30:26 +0700

    ssss

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -1,6 +1,6 @@
 // backend/src/controllers/orderController.ts
 import { Request, Response } from "express";
-import { Order } from "../models/Order";
+import Order from "../models/Order";
 
 export const createOrder = async (req: Request, res: Response) => {
     try {
